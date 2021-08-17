f72bb7a865cc864886f67915965fa9e66b8f82af
dell <dell@DESKTOP-RMMGDGD>
2021-08-17 12:34:04 +0530

    Cust_info commit

---
+++ demo1/src/main/java/demo1/Cust_info.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Cust_info {
+
+	public static void main(String[] args) {
+		System.out.println("Hellooo!!!");
+		System.out.println("Contains Customer Details.");
+
+	}
+
+}
